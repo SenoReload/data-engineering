f0b8116a5dab131810758285c79e9bc58faa9ff7
SenoReload <ad_con.reload@protonmail.ch>
2023-05-29 19:50:53 -0700

    feat: :sparkles: add chapter 1

---
+++ Ch1_GettingStarted.py
@@ -0,0 +1,10 @@
+# Zen of Python
+import this
+
+# Write a Hello World message
+message = "hElLo WoRlD!"
+
+print(message.title()) # Title case
+print(message.upper()) # Upper case
+print(message.lower()) # Lower case
+
